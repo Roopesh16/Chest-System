53276faf4acd1bb66d9668d335d9e1c9075b422a
Roopesh Verma <roopeshv.30@gmail.com>
2024-04-13 12:23:22 +0530

    Add Game Service

---
+++ Assets/Scripts/Main/GameService.cs
@@ -0,0 +1,27 @@
+using ChestSystem.Chest;
+using ChestSystem.UI;
+using ChestSystem.Utilities;
+using UnityEngine;
+
+namespace ChestSystem.Main
+{
+    public class GameService : GenericMonoSingleton<GameService>
+    {
+        #region --------- Serialized Variables ---------
+        [SerializeField] private UIService uiService;
+        #endregion ------------------
+        
+        #region --------- Public Variables ---------
+        public ChestService ChestService { get; private set; }
+        public UIService UIService => uiService;
+        #endregion ------------------
+
+        #region --------- Monobehavior Methods ---------
+        private void Awake()
+        {
+            base.Awake();
+            ChestService = new ChestService();
+        }
+        #endregion ------------------
+    }
+}
