c3739dd5970842275a5440cde9fbf080f641f68a
Roopesh Verma <roopeshv.30@gmail.com>
2024-04-14 20:47:45 +0530

    Set open chest stage

--- Assets/Scripts/Chest/ChestController.cs
+++ Assets/Scripts/Chest/ChestController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using ChestSystem.ScriptableObjects;
 using DG.Tweening;
@@ -18,30 +19,49 @@ namespace ChestSystem.Chest
         #endregion ------------------
         
         #region --------- Private Methods ---------
+
+        private void InitializeModel(ChestScriptableObject chestScriptableObject)
+        {
+            chestModel = new ChestModel(chestScriptableObject);
+            chestModel.SetController(this);
+        }
         
         private void InitializeView(ChestView chestPrefab, Transform parent,int siblingIndex)
         {
-            chestView = Object.Instantiate(chestPrefab,parent);
+            chestView = GameObject.Instantiate(chestPrefab,parent);
+            chestView.SetController(this);
             chestView.SetupChestSlots(chestModel.ChestName,chestModel.ClosedChestSprite);
             chestView.transform.SetSiblingIndex(siblingIndex);
         }
+
+        private void SetOpenImage()
+        {
+            chestView.SetOpenChestImage(chestModel.OpenChestSprite);
+            TimeSpan timeSpan = TimeSpan.FromSeconds(chestModel.TimerSecs);
+            string time = string.Format("{0:D2}:{1:D2}:{2:D2}",timeSpan.Hours,timeSpan.Minutes,timeSpan.Seconds);
+            chestView.SetUnlockTimer(time);
+        }
+        #endregion ------------------
+        
+        #region --------- Protected Methods ---------
         #endregion ------------------
 
         #region --------- Public Methods ---------
 
-        public void Init(ChestScriptableObject chestScriptableObject,ChestView chestPrefab,Transform parent,int siblingIndex)
+        public ChestController(ChestScriptableObject chestScriptableObject,ChestView chestPrefab,Transform parent,int siblingIndex)
         {
-            chestModel = new ChestModel(chestScriptableObject);
+            InitializeModel(chestScriptableObject);
             InitializeView(chestPrefab,parent,siblingIndex);
         }
-
+        
         public void ShakeChestSprite()
         {
             Transform imgTransform = chestView.GetChestImage().transform;
             imgTransform.DOShakePosition(chestModel.ShakeDuration,chestModel.ShakeStrength);
             imgTransform.DOShakeScale(chestModel.ShakeDuration, chestModel.ShakeStrength);
+
+            SetOpenImage();
         }
-            
         #endregion ------------------
     }
 }
