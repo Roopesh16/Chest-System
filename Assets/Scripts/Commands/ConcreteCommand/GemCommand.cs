82698a504a2855abd74a8b1affc9a675f4b0959b
Roopesh Verma <roopeshv.30@gmail.com>
2024-04-18 15:27:17 +0530

    Update execute and undo method

--- Assets/Scripts/Commands/ConcreteCommand/GemCommand.cs
+++ Assets/Scripts/Commands/ConcreteCommand/GemCommand.cs
@@ -1,18 +1,33 @@
 using ChestSystem.Main;
 using ChestSystem.Transaction;
 using ChestSystem.Commands.Abstract;
+using ChestSystem.Chest;
+using ChestSystem.UI;
 
 namespace ChestSystem.Commands.Concrete
 {
     public class GemCommand : ChestCommand
     {
         private TransactionService TransactionService => GameService.Instance.TransactionService;
+        private UIService UIService => GameService.Instance.UIService;
 
-        public GemCommand(int gemCount) => this.gemCount = gemCount;
+        private ChestController chestController;
+        public GemCommand(ChestController chestController, int gemCount)
+        {
+            this.chestController = chestController;
+            this.gemCount = gemCount;
+        }
+        public override void Execute() 
+        { 
+            TransactionService.DeductGems(gemCount);
+            UIService.SwitchUndoBtn(true);
+        }
 
-        public override void Execute() =>  TransactionService.DeductGems(gemCount);
-
-        public override void Undo() => TransactionService.AddCoinGemCount(gemCount);
+        public override void Undo() 
+        {
+            TransactionService.AddCoinGemCount(gemCount);
+            chestController.SetLockedState();
+        } 
     }
 }
 
